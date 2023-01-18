a608c86dc0196cf6cf31a711ce8e2eb140b3c5b0
Carlos <github.com/CarlosMartinezCM>
2023-01-18 06:49:48 -0800

    added refresh button and function comments

--- components/WeatherApp.js
+++ components/WeatherApp.js
@@ -1,3 +1,4 @@
+
 class WeatherForecast extends React.Component {
     constructor(props) {
         super(props);
@@ -6,9 +7,9 @@ class WeatherForecast extends React.Component {
                 };
     }
     componentDidMount = () => {
-        this.getForecast();
+        this.getCurrentWeather();
     }
-    getForecast = async() => {
+    getCurrentWeather = async() => {
         const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat='+
          this.state.latitude + '&lon=' +
          this.state.longitude + '&appid=26a5f25cabf9b11d8b970976611bc138');
@@ -40,6 +41,10 @@ toggleUnits = () => {
         this.setState({tempUnit: "F", temp: Math.round((this.state.temp *9/5) + 32)});
         {this.state.tempUnit};
     }
+    //Udate the current weather conditions
+    updateWeather = () => {
+        this.getCurrentWeather();        
+    }
     render() {
         return (
             <div>
@@ -47,14 +52,17 @@ toggleUnits = () => {
                 <h2> {this.state.place}</h2>
                 <h6>Lat: {this.state.latitude} Lon: {this.state.longitude}</h6>           
                 <h5>{this.state.conditions}</h5>
-                <h3>{(this.state.temp *9/5) + 32} F</h3>
-                <h5>{this.state.temp} C</h5>
+                <h3>{(this.state.temp *9/5) + 32}&deg;&nbsp; F</h3>
+                <h5>{this.state.temp}&deg;&nbsp; C</h5>
                 <h6>Humidity {this.state.humidity}%</h6>
                 <h6>Wind Speed {this.state.wind} MPH</h6>
                 <h6>Barometer {this.state.barometer} </h6>
                 <h6>Visibility {this.state.visibility + " " + this.state.visibilityUnits}</h6>
                 <h6>Wind Chill </h6>
                 <h6><i>Last Updated on {this.state.retrieved}</i></h6>
+                <div className="refresh button">
+                    <input id="refresh" type="button" value="refresh" onClick={this.updateWeather} />
+                </div>
             </div>
         );
     }
@@ -69,20 +77,26 @@ class CurrentWeather extends React.Component {
         this.state = {station: null};
     }
     componentDidMount = () => {
+        //Get users location if allowed
         navigator.geolocation.getCurrentPosition(this.getLocSuccess,this.getLocError);
     }
+    //If the user allows to get location, the weather is set to the users current GeoLocation position. 
     getLocSuccess = (position) => {
         this.setState({station: {lat: position.coords.latitude, long: position.coords.longitude}});       
     }
+    //This fucntion is called if the user denies to give their current Geolocation and initializes the default location. 
     getLocError = (err) => {
-        this.setState({station: {lat: 46.26955, long: 119.11813}});
+        this.setState({station: {lat: 46.26955, long: -119.11813}});
     }
+    //Instantiating WeatherForecast components in CurrentWeather to pass in functions as props
     render() {
         if(this.state.station != null) {
             return (
             <div id="main">
                 <div align="center" className="jumbotron">
-                    <WeatherForecast latitude={this.state.station.lat} longitude={this.state.station.long} />
+                    <WeatherForecast
+                     latitude={this.state.station.lat}
+                     longitude={this.state.station.long} />
                 </div>
             </div>
             );
