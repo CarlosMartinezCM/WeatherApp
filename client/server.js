c0afa9084ee55ef817b25f94790aeb0e8d968489
unknown <https://github.com/CarlosMartinezCM>
2023-05-15 19:52:50 -0700

    quick fix

--- client/server.js
+++
@@ -1,9 +0,0 @@
-// server.js -- This is an express.js web server for serving React apps. 
-import path from 'path';
-import express from 'express';
-const PORT = process.env.HTTP_PORT || 8081;
-const app = express();
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.listen(PORT, () => {
-    console.log(`Server listening at port ${PORT}.`);
-});  
