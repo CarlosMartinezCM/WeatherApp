ac6092e11b7137116fdcd82a4ec35ffe48982fa0
Carlos Martinez <carlos.martinez@ksd.org>
2024-01-12 13:24:18 -0800

    changed the background color for better viewing and converted units

--- client/src/components/CurrentWeather.js
+++ client/src/components/CurrentWeather.js
@@ -38,93 +38,6 @@ class WeatherForecast extends React.Component {
         }
     }
 
-    //This function will make an API call to the NOAA API weather service and fetch forecast data
-    getWeatherForecast = async () => {
-        const response = await fetch(`https://api.weather.gov/points/${this.state.latitude},${this.state.longitude}`);
-        const data = await response.json();
-        const forecastLink = data.properties.forecast;
-        const forecastResponce = await fetch(forecastLink);
-        const forecastData = await forecastResponce.json();
-        const obs = data.properties.observationStations;
-        const observationResponce = await fetch(obs);
-        const observationData = await observationResponce.json();
-
-        this.setState({
-            city: data.properties.relativeLocation.properties.city,
-            state: data.properties.relativeLocation.properties.state,
-            station: data.properties.cwa,
-            gridX: data.properties.gridX,
-            gridY: data.properties.gridY,
-            gridId: data.properties.gridId,
-            forecast: forecastData.properties.periods[0],
-            forecastPeriods: forecastData.properties.periods,
-            temperature: forecastData.properties.periods[0].temperature,
-            temperatureUnit: forecastData.properties.periods[0].temperatureUnit,
-            isDaytime: forecastData.properties.periods[0].isDaytime,
-            windDirection: forecastData.properties.periods[0].windDirection,
-            windSpeed: forecastData.properties.periods[0].windSpeed,
-            shortForecast: forecastData.properties.periods[0].shortForecast,
-            forecastHourly: forecastData.properties.periods[0],
-            units: forecastData.properties.units,
-            updated: forecastData.properties.updated,
-            stationIdentifier: observationData.features[0].properties.stationIdentifier,
-        })
-
-
-        //this.getCurrentObservations(); 
-    }
-
-    getCurrentObservations = async () => {
-
-        try {
-            const response = await fetch(`https://api.weather.gov/stations/${this.state.stationIdentifier}/observations/latest`);
-            const data = await response.json();
-
-            this.setState({
-                textDescription: data.properties.textDescription,
-                temp: data.properties.temperature.value,
-                icon: data.properties.icon,
-                elev: data.properties.elevation.value,
-                unitCode: data.properties.elevation.unitCode[8],
-                relativeHumidity: data.properties.relativeHumidity.value,
-                windSpeed: data.properties.windSpeed.value,
-                barometricPressure: data.properties.barometricPressure.value,
-                dewpoint: data.properties.dewpoint.value,
-                //visibility is in meters need to convert it
-                visibility: data.properties.visibility.value,
-                //WindChill is in C need to convert it
-                windChill: data.properties.windChill.value,
-                //Timestamp needs to be converted
-                timestamp: data.properties.timestamp,
-            });
-        } catch (error) {
-            console.error('Error fetching current observations:', error);
-        }
-        // console.log('is daytime',this.state.isDaytime);
-    }
-
-    toggleUnits = () => {
-        if (this.state.tempUnit === "F") {
-            this.setState({ tempUnit: "C", temp: Math.round((this.state.temp - 32) * 5 / 9) });
-        } else {
-            this.setState({ tempUnit: "F", temp: Math.round((this.state.temp * 9 / 5) + 32) });
-        }
-    }
-    convertUnitsF = () => {
-        this.setState({ tempUnit: "F", temp: Math.round((this.state.temp * 9 / 5) + 32) });
-        // { this.state.tempUnit };
-    }
-    //Udate the current weather conditions
-    updateWeather = async () => {
-        this.getWeatherForecast();
-        //alert("updating weather");
-    }
-
-    handleChange = (event) => {
-        event.preventDefault();
-        this.props.changeMode(AppMode.WEATHER);
-    }
-
     handleForecastClick = async () => {
         try {
             this.setState({ loading: true });
@@ -165,12 +78,100 @@ class WeatherForecast extends React.Component {
         alert("Alert Radar");
     };
 
+    //This function will make an API call to the NOAA API weather service and fetch forecast data
+    getWeatherForecast = async () => {
+        const response = await fetch(`https://api.weather.gov/points/${this.state.latitude},${this.state.longitude}`);
+        const data = await response.json();
+        const forecastLink = data.properties.forecast;
+        const forecastResponce = await fetch(forecastLink);
+        const forecastData = await forecastResponce.json();
+        const obs = data.properties.observationStations;
+        const observationResponce = await fetch(obs);
+        const observationData = await observationResponce.json();
+
+        this.setState({
+            city: data.properties.relativeLocation.properties.city,
+            state: data.properties.relativeLocation.properties.state,
+            station: data.properties.cwa,
+            gridX: data.properties.gridX,
+            gridY: data.properties.gridY,
+            gridId: data.properties.gridId,
+            forecast: forecastData.properties.periods[0],
+            forecastPeriods: forecastData.properties.periods,
+            temperature: forecastData.properties.periods[0].temperature,
+            temperatureUnit: forecastData.properties.periods[0].temperatureUnit,
+            isDaytime: forecastData.properties.periods[0].isDaytime,
+            windDirection: forecastData.properties.periods[0].windDirection,
+            windSpeed: forecastData.properties.periods[0].windSpeed,
+            shortForecast: forecastData.properties.periods[0].shortForecast,
+            forecastHourly: forecastData.properties.periods[0],
+            units: forecastData.properties.units,
+            updated: forecastData.properties.updated,
+            stationIdentifier: observationData.features[0].properties.stationIdentifier,
+        })
+
+    }
+    //This function will make an API call to the NOAA API weather service and fetch current observation data
+    getCurrentObservations = async () => {
+        try {
+            const response = await fetch(`https://api.weather.gov/stations/${this.state.stationIdentifier}/observations/latest`);
+            const data = await response.json();
+
+            this.setState({
+                textDescription: data.properties.textDescription,
+                temp: data.properties.temperature.value,
+                icon: data.properties.icon,
+                elev: data.properties.elevation.value,
+                unitCode: data.properties.elevation.unitCode[8],
+                relativeHumidity: data.properties.relativeHumidity.value,
+                windSpeed: data.properties.windSpeed.value,
+                barometricPressure: data.properties.barometricPressure.value,
+                dewpoint: data.properties.dewpoint.value,
+                visibility: data.properties.visibility.value,
+                windChill: data.properties.windChill.value,
+                timestamp: data.properties.timestamp,
+            });
+        } catch (error) {
+            console.error('Error fetching current observations:', error);
+        }
+    }
+    //Toggle units from F to C, not yet implemented.    
+    toggleUnits = () => {
+        if (this.state.tempUnit === "F") {
+            this.setState({ tempUnit: "C", temp: Math.round((this.state.temp - 32) * 5 / 9) });
+        } else {
+            this.setState({ tempUnit: "F", temp: Math.round((this.state.temp * 9 / 5) + 32) });
+        }
+    }
+    //convert the timestamp to a readable format before rendering
+    formatTimestamp = (timestamp) => {
+        const timestampUTC = new Date(timestamp);
+        const formattedTimestamp = timestampUTC.toLocaleString('en-US', {
+            month: 'short',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+            timeZoneName: 'short',
+            timeZone: 'America/Los_Angeles',
+        });
+        return formattedTimestamp;
+    };
+    //Udate the current weather conditions
+    updateWeather = async () => {
+        this.getWeatherForecast();
+        // alert("updating weather");
+    }
+    handleChange = (event) => {
+        event.preventDefault();
+    }
     render() {
         const { icon } = this.state;
         const { forecast } = this.state;
         if (!forecast) {
             return <div>No data available. Please refresh the page!</div>
         }
+        const formattedTimestamp = this.formatTimestamp(this.state.timestamp);
         return (
             <div>
                 <div class="current-heading">
@@ -212,25 +213,26 @@ class WeatherForecast extends React.Component {
                     </div>
                     <div class="center-current-data">
                         <div class="center-data-item">
-                            <strong>Humidity:</strong> {Math.round(this.state.relativeHumidity)}
+                            <strong>Humidity:</strong> {Math.round(this.state.relativeHumidity)}%
                         </div>
                         <div class="center-data-item">
-                            <strong>Wind Speed:</strong> {this.state.windSpeed}
+                            <strong>Wind Speed:</strong> {this.state.windDirection} {(Math.round(this.state.windSpeed * .621371))} MPH
                         </div>
                         <div class="center-data-item">
-                            <strong>Barometer:</strong> {this.state.barometricPressure}
+                            {/* Convert to inches and round to nearest tenth then convert to millibars */}
+                            <strong>Barometer:</strong> {((this.state.barometricPressure * .0002953).toFixed(2))} in ({(this.state.barometricPressure / 100).toFixed(2)}mb)
                         </div>
                         <div class="center-data-item">
-                            <strong>Dewpoint:</strong> {this.state.dewpoint}
+                            <strong>Dewpoint:</strong> {Math.round((this.state.dewpoint * 9 / 5) + 32)}°F ({Math.round(this.state.dewpoint)}°C)
                         </div>
                         <div class="center-data-item">
-                            <strong>Visibility:</strong> {this.state.visibility}
+                            <strong>Visibility:</strong> {(Math.round(this.state.visibility * .000621371).toFixed(2))} mi
                         </div>
                         <div class="center-data-item">
-                            <strong>Wind Direction:</strong> {this.state.windDirection}
+                            <strong>Wind Chill:</strong> { }{Math.round((this.state.windChill * 9 / 5) + 32)}°F ({Math.round(this.state.windChill)}°C)
                         </div>
                         <div class="center-data-item">
-                            <strong>Last updated:</strong> {this.state.timestamp}
+                            <strong>Last updated:</strong> {formattedTimestamp}
                         </div>
                     </div>
                 </div>
@@ -253,9 +255,9 @@ class WeatherForecast extends React.Component {
                                 {/* Conditionally check if it is daytime or night time to get the High or Low temperature*/}
                                 {period.isDaytime ? (
                                     <div>
-                                        <p>High {period.temperature} °{period.temperatureUnit}</p>
+                                        <p>High: {period.temperature} °{period.temperatureUnit}</p>
                                     </div>) : (
-                                    <p>Low {period.temperature} °{period.temperatureUnit}</p>)}
+                                    <p>Low: {period.temperature} °{period.temperatureUnit}</p>)}
                                 <p></p>
                             </div>
                         ))}
@@ -332,6 +334,10 @@ class CurrentWeather extends React.Component {
             } else { alert("no station for this loation"); }
         }
     }
+    handleChange = (event) => {
+        event.preventDefault();
+        this.props.changeMode(AppMode.SITEMAP);
+    }
     //Instantiating WeatherForecast components in CurrentWeather to pass in functions as props
     //To implement the hourly I should create another page using an api to retrieve that info and display the hourly info
     //or try doing what weather channel does and 
@@ -339,20 +345,28 @@ class CurrentWeather extends React.Component {
         if (this.state.station != null) {
             return (
                 <div id="main">
-                    <div class="cardSearch" align="left" >
-                        <form >
-                            <input
-                                ref={this.citySearch}
-                                type="text"
-                                id="cName"
-                                name="cName"
-                                placeholder="Enter City name" />
-                            <button
-                                type="submit"
-                                class="btn-color-theme" onClick={this.searchLocation} >
-                                <span id="login-btn-icon" className={this.state.searchButton} />&nbsp;{this.state.searchButton}
-                            </button>
-                        </form>
+                    <div class="container">
+                        <div class="cardSearch" >
+                            <form >
+                                <input
+                                    ref={this.citySearch}
+                                    type="text"
+                                    id="cName"
+                                    name="cName"
+                                    placeholder="Enter City name" />
+                                <button
+                                    type="submit"
+                                    class="btn-color-theme" onClick={this.searchLocation} >
+                                    <span id="login-btn-icon" className={this.state.searchButton} />&nbsp;{this.state.searchButton}
+                                </button>
+                            </form>
+                        </div>
+                        <div className="moreInfo">
+                            <p>More Information:</p>
+                            <div class="infoList"
+                                onClick={this.handleChange} >&nbsp;Site Map
+                            </div>
+                        </div>
                     </div>
                     <div class="card" >
                         <WeatherForecast latitude={this.state.station.lat} longitude={this.state.station.lon} />
--- client/src/components/SiteMap.js
+++ client/src/components/SiteMap.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AppMode from "./../AppMode";
 
 class SiteMap extends React.Component {
     constructor() {
@@ -7,12 +8,18 @@ class SiteMap extends React.Component {
             color: "black"
         };
     }
+    handleChange = (event) => {
+        event.preventDefault();
+        this.props.changeMode(AppMode.WEATHER);
+    }
     render() {
         return (
             <div >
                     <div class="navOptions">
                             <div className="navOptions"
                                 onClick={this.props.homeweatherPage} >&nbsp;Weather</div>
+                                {/*<div className="navOptions"
+                                onClick={this.handleChange} >&nbsp;Weather using handle change</div>*/}
                     </div>
             </div>
         )
